01650d843a7db0286c0a2436c14c8af06e74c387
GenesisMauries <exodo_54@hotmail.com>
2019-03-30 09:09:04 -0600

    Sube buscador completo

---
+++ src/data/items.js
@@ -0,0 +1,122 @@
+let id = 1;
+
+export default [
+    {
+        id: id++,
+        title: `Laboratoria Coding`,
+        image: `https://cdn-images-1.medium.com/max/1200/1*3ebT5azEfTrKxANOE3BwyA.jpeg`,
+        content: `Trabajamos para que miles de mujeres en América Latina desarrollen carreras en tecnología que cambien su futuro y hagan más diversa a la industria.
+        A través de nuestro Bootcamp mujeres que no tuvieron acceso a una educación de calidad, se convierten en Desarrolladoras Web y Diseñadoras de Experiencia de Usuario, accediendo a transformadores trabajos en tecnología..`,
+        page: `https://www.laboratoria.la/`
+    },
+    {
+        id: id++,
+        title: `Laboratorio Xotepingo`,
+        image: `https://laboratorioxotepingo.com/images/partners_grey/logoxotepingo.png`,
+        content: `Sabemos que el bienestar de la familia depende del estado de salud de cada uno de sus miembros, de modo que cuidar a mamá y a papá se vuelve prioridad.
+        Hoy en día tenemos disponibles los recursos tecnológicos necesarios para prevenir enfermedades y conservar la salud, obteniendo una atención médica temprana y oportuna.
+        Creemos que la cultura de prevención y detección temprana de enfermedades es una de las prácticas que mejoran la calidad de vida.
+        Por eso nuestro objetivo es ofrecer siempre el mejor servicio y dar a nuestros pacientes un diagnóstico preciso y confiable.`,
+        page: `https://laboratorioxotepingo.com/index.html`,
+    },
+    {
+        id: id++,
+        title: `Nissan`,
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNO58WZpjjazisWto1xQuebKdkIiUZt6iZyxNo3-I_Vs3pKcYh`,
+        content: `Proveer productos y servicios automotrices únicos e innovadores que ofrecen valores medibles y superiores, en alianza con Renault, a todos nuestros acreedores*.
+        *Nuestros acreedores incluyen clientes, accionistas, empleados, distribuidores, proveedores, así como las comunidades donde trabajamos y tenemos operaciones. La sociedad en general ha sido acreedora de Nissan, en medio ambiente, seguridad, etc.`,
+        page: `https://www.nissan.com.mx/`,
+    },
+    {
+        id: id++,
+        title: `Taqueria El Califa`,
+        image: `http://www.elcalifa.com.mx/web/wp-content/uploads/2017/12/logocalifa18.jpg`,
+        content: `Hace 25 años El Califa dio un giro al concepto tradicional de las Taquerías en Mésico, hoy volvemos a evolucionar y pronto serás parte de esto.`,
+        page: `http://www.elcalifa.com.mx/web/`
+    },
+    {
+        id: id++,
+        title: `LG`,
+        image: `https://www.lg.com/lg4-common-gp/img/global/header-large-logo.png`,
+        content: `LG Electronics te permite realizar tus sueños ofreciendo grandes oportunidades de desarollo profesional`,
+        page: `https://www.lg.com/mx`,
+    },
+    {
+        id: id++,
+        title: `La Salle`,
+        image: `http://www.lasalle.mx/wp-content/uploads/2014/03/LogoLASALLE_V2013-01.png`,
+        content: `El origen en el tiempo y el espacio de la Universidad La Salle, se remonta al Siglo XVII en Francia donde Juan Bautista De La Salle funda en 1680 el Instituto de los Hermanos de las Escuelas Cristianas, para dar educación cristiana, escolar y sobre todo humana a los niños y jóvenes desfavorecidos de aquella época.`,
+        page: `https://www.lasalle.mx/`,
+    },
+    {
+        id: id++,
+        title: `Hospital Sedna`,
+        image: `http://www.hospitalsedna.com/images/logo/GetAttachment.aspx.jpeg`,
+        content: `Hospital  Sedna es un hospital privado ubicado al Sur de la Ciudad de México donde  ofrecemos la mejor  atención médica a todos los pacientes tanto nacionales como extranjeros, para ello contamos con personal altamente calificado. Nuestro equipo, con un inmejorable espacio y la más alta tecnología en beneficio de la salud, se dirige a una misma meta: el servicio y atención médica de excelencia.`,
+        page: `http://www.hospitalsedna.com/es/`,
+    },
+    {
+        id: id++,
+        title: `Monarquia Coronado`,
+        image: `http://www.monarquiacoronado.com.mx/_include/img/log_Monarquia.png`,
+        content: `Contamos con quince salones diferentes que se adaptan a tus necesidades, así que si deseas un evento pequeño, una conferencia, convención, graduación, boda, quince años, comidas y eventos empresariales, seguramente encontrarás aquí la respuesta a lo que necesitas`,
+        page:`http://www.monarquiacoronado.com.mx/`,
+    },
+    {
+        id: id++,
+        title: `Reforma`,
+        image: `https://www.reforma.com/libre/imgdiseno/header/3.png`,
+        content: `El periódico Reforma, Corazón de México es un diario de circulación nacional en México que se imprime en la Ciudad de México y es editado por el Grupo Reforma. Circula desde el 20 de noviembre de 1993.`,
+        page:`https://www.reforma.com/`,
+    },
+    {
+        id: id++,
+        title: `TV Notas`,
+        image: `http://www.tvnotas.com.mx/sites/default/files/logotipo/logotipo_1485989027.png`,
+        content: `TVNotas sitio web de espectáculos más leído en todo México con noticias minuto a minuto, exclusivas, entretenimiento, horóscopos, consejos, recetas, ...`,
+        page: `http://www.tvnotas.com.mx/`,
+    },
+    {
+        id: id++,
+        title: `Intelimetrica`,
+        image: ``,
+        content: `We build products and offer services that empower organizations to make better decisions faster and at scale. We lever data, technology, and analytics to build customized solutions that solve real world problems in real world contexts.`,
+        page:`https://intelimetrica.com/`
+    },
+    {
+        id: id++,
+        title: `Secretaria del medio ambiente`,
+        image: `https://www.sedema.cdmx.gob.mx/themes/base/assets/images/logos/Logo_Dependencia.png`,
+        content: `La Secretaría del Medio Ambiente es una dependencia de la administración pública del Gobierno de la Ciudad de México dependiente del Jefe de Gobierno, que tiene a su cargo la formulación, ejecución y evaluación de la política del Distrito Federal en materia ambiental y de recursos naturales..`,
+        page:`https://www.sedema.cdmx.gob.mx/`,
+    },
+    {
+        id: id++,
+        title: `Bimbo`,
+        image: `https://grupobimbo.com/sites/default/files/grupo-bimbo-logotipo_0.svg`,
+        content: `Grupo Bimbo S.A.B. de C.V. es una empresa multinacional mexicana, y la de panificación más grande en el mundo​ con operaciones en América, Asia, África y Europa.​ Desde 1980, sus acciones cotizan en la Bolsa Mexicana de Valores con la clave de pizarra BIMBO y BIMBOA.`,
+        page:`https://grupobimbo.com/es`,
+    },
+    {
+        id: id++,
+        title: `Nomada`,
+        image: `http://nomada-e.com/img/NoMADA_Black.png`,
+        content: `Es una Tarjeta de Grado Industrial para desarrollo de Aplicaciones Mecatrónicas, Sistemas Embebidos e Internet de las Cosas; totalmente compatible con la plataforma de desarrollo de Atmel®.
+        Además de contar con acabados Premium en Oro para mejorar su rendimiento eléctrico y térmico, tiene características únicas que la vuelven, sin duda alguna, la mejor opción del Mercado.`,
+        page:`http://nomada-e.com/`
+    },
+    {
+        id: id++,
+        title: `Kryo`,
+        image: `https://kryo.rocks/images/kryo_256x256.png`,
+        content: `Kryo es un pedal de guitarra capaz de ser reprogramado con un gran número de efectos (a través de una aplicación móvil). ¡Existe una gran cantidad de sonidos listos para hacerte rockear! #FeelTheEffect`,
+        page:`https://kryo.rocks/`
+    },
+    {
+        id: id++,
+        title: `Happinss`,
+        image: `https://happinss.com/en/img/happinss_logo.png`,
+        content: `HAPPINSS allows you to contemplate the world like never before, a window to the world where you can explore your inner self, it enhances a sense of freedom and connection with your surrounding, a mindfulness experience in spectacular nature destinations.`,
+        page:`https://happinss.com/en/index.html`,
+    },
+];
